e10d56276bc97f8487eead05595640fc51912078
Victor Deng <victor.deng@epfl.ch>
2025-04-10 10:56:34 +0200

    Strengthen invariant with a list of open groups
    Some little details still need to be fixed/proved in the case of groups; the case of repeated matching has not yet been proved

--- warblreequiv/TMatching.v
+++ warblreequiv/TMatching.v
@@ -305,62 +305,77 @@ Fixpoint reset_groups_ms (gidl: list Groups.group_id) (s: MatchState): Result Ma
     reset_groups_ms q (match_state inp endInd cap)
   end.
 
+(* A list of currently open groups *)
+Definition open_groups := list (Groups.group_id * integer).
+
+Fixpoint has_group (id: Groups.group_id) (gl: open_groups): bool :=
+  match gl with
+  | nil => false
+  | (id', _)::q => if id == id' then true else has_group id q
+  end.
+
+Fixpoint close_group (id: Groups.group_id) (gl: open_groups) end_index: Result (CaptureRange * open_groups) tree_res'_error :=
+  match gl with
+  | nil => Error MismatchingTree
+  | (id', start)::q =>
+    if id == id' then
+      Success (capture_range start end_index, q)
+    else
+      let! (range, q') =<< close_group id q end_index in
+      Success (range, (id', start)::q')
+  end.
+
 (* TODO Direction *)
-Definition group_effect' (g: groupaction) (s: MatchState): Result MatchState tree_res'_error :=
+Definition group_effect' (g: groupaction) (s: MatchState) (gl: open_groups): Result (MatchState * open_groups) tree_res'_error :=
   match g with
   | Open gid =>
-      let cap := MatchState.captures s in
-      set cap[gid] := capture_range (MatchState.endIndex s) (-1)%Z in
-      Success (match_state (MatchState.input s) (MatchState.endIndex s) cap)
+      Success (s, (gid, MatchState.endIndex s)::gl)
   | Close gid =>
       let cap := MatchState.captures s in
-      let! range =<< cap[gid] in
-      match range with
-      | None => Error MismatchingTree
-      | Some (capture_range x y) =>
-        set cap[gid] := capture_range x (MatchState.endIndex s) in
-        Success (match_state (MatchState.input s) (MatchState.endIndex s) cap)
-      end
+      let! (range, gl') =<< close_group gid gl (MatchState.endIndex s) in
+      set cap[gid] := range in
+      Success (match_state (MatchState.input s) (MatchState.endIndex s) cap, gl')
   | Reset gidl =>
-      reset_groups_ms gidl s
+      let! s' =<< reset_groups_ms gidl s in
+      Success (s', gl)
   end.
 
 (* returning the highest-priority result *)
 (* we also return the corresponding state *)
 (* TODO Direction *)
-Fixpoint tree_res' (t:tree) (s: MatchState): Result (option MatchState) tree_res'_error :=
+Fixpoint tree_res' (t:tree) (s: MatchState) (gl: open_groups): Result (option MatchState) tree_res'_error :=
   match t with
   | Mismatch => Success None
   | Match => Success (Some s)
   | Choice t1 t2 =>
-      let! res1 =<< tree_res' t1 s in
+      let! res1 =<< tree_res' t1 s gl in
       match res1 with
-      | None => tree_res' t2 s
+      | None => tree_res' t2 s gl
       | Some _ => Success res1
       end
   | Read c t1 => 
     if List.nth (Z.to_nat (MatchState.endIndex s)) (MatchState.input s) c == c then
-      tree_res' t1 (advance_ms s)
+      tree_res' t1 (advance_ms s) gl
     else
       Error MismatchingTree
   | CheckFail _ => Success None
-  | CheckPass _ t1 => tree_res' t1 s
+  | CheckPass _ t1 => tree_res' t1 s gl
   | GroupAction g t1 => 
-    let! s' =<< group_effect' g s in tree_res' t1 s'
+    let! (s', gl') =<< group_effect' g s gl in tree_res' t1 s' gl'
   end.
 
 
-Definition equiv_tree (mc: MatcherContinuation) (tmc: TMatcherContinuation) :=
+Definition equiv_tree (mc: MatcherContinuation) (tmc: TMatcherContinuation) (gl: open_groups) :=
   forall s: MatchState, 
   match tmc s, mc s with
   | Error _, _ | _, Error _ => True (* finer modeling? *)
-  | Success t, Success res => tree_res' t s = Success res
+  | Success t, Success res => tree_res' t s gl = Success res
   end.
 
 Definition id_mcont: MatcherContinuation := fun x => Success (Some x).
 Definition id_tmcont: TMatcherContinuation := fun _ => Success Match.
 
-Lemma id_equiv: equiv_tree id_mcont id_tmcont.
+Lemma id_equiv: equiv_tree id_mcont id_tmcont nil.
 Proof.
   intro s. simpl. reflexivity.
 Qed.
@@ -372,7 +387,7 @@ Theorem compile_tcompile: forall reg ctx rer,
   match m', tm' with
   | Error _, _ | _, Error _ => True
   | Success m, Success tm =>
-  forall mc tmc, equiv_tree mc tmc -> equiv_tree (fun s => m s mc) (fun s => tm s tmc)
+  forall mc tmc gl, equiv_tree mc tmc gl -> equiv_tree (fun s => m s mc) (fun s => tm s tmc) gl
   end.
 Proof.
   intros reg ctx rer.
@@ -450,8 +465,8 @@ Proof.
     simpl in *.
     intros.
     intro s'.
-    specialize (IHreg1 mc tmc H s').
-    specialize (IHreg2 mc tmc H s').
+    specialize (IHreg1 mc tmc gl H s').
+    specialize (IHreg2 mc tmc gl H s').
     simpl in *.
     destruct (tm1 s' tmc) as [t1|] eqn:?; simpl; try exact I.
     destruct (tm2 s' tmc) as [t2|] eqn:?; simpl; try exact I.
@@ -475,8 +490,8 @@ Proof.
     destruct (tCompileSubPattern reg2 _ _ _) as [tm2|] eqn:?; simpl; try exact I.
     simpl in *.
     intros. intro.
-    specialize (IHreg2 mc tmc H).
-    specialize (IHreg1 (fun s0 => m2 s0 mc) (fun s0 => tm2 s0 tmc) IHreg2 s).
+    specialize (IHreg2 mc tmc gl H).
+    specialize (IHreg1 (fun s0 => m2 s0 mc) (fun s0 => tm2 s0 tmc) gl IHreg2 s).
     simpl in *.
     assumption.
   - (* Group *)
@@ -485,10 +500,40 @@ Proof.
     specialize (IHreg ctx').
     destruct (Semantics.compileSubPattern reg ctx' rer forward) as [m|] eqn:?; simpl; try exact I.
     destruct (tCompileSubPattern reg ctx' rer forward) as [tm|] eqn:?; simpl; try exact I.
-    intros mc tmc H s.
+    intros mc tmc gl H s.
     simpl in *.
-    
-    admit.
+    remember (fun y : MatchState => _) as treecont.
+    remember (fun y : MatchState => let! r =<< _ in let! cap =<< _ in mc _) as origcont.
+    remember (StaticSemantics.countLeftCapturingParensBefore _ ctx + 1) as gid.
+    set (gl' := (gid, MatchState.endIndex s)::gl).
+    specialize (IHreg origcont treecont gl').
+    assert (equiv_tree origcont treecont gl') as Hequivcont.
+    {
+      intro y.
+      rewrite Heqtreecont, Heqorigcont.
+      remember (MatchState.endIndex s) as x.
+      destruct negb eqn:?; simpl; try exact I.
+      destruct (gid =? 0) eqn:?; simpl; try exact I.
+      destruct List.List.Update.Nat.One.update eqn:?; simpl; try exact I.
+      remember (match_state _ _ s0) as s'.
+      destruct (tmc s') as [t|] eqn:?; simpl; try exact I.
+      destruct (mc s') as [res|] eqn:?; simpl; try exact I.
+      rewrite EqDec.reflb. simpl.
+      rewrite Heqb0.
+      (* Prove: the result of List.List.....Update does not depend on the type of the error that should be returned if any *)
+      replace (@List.List.Update.Nat.One.update _ tree_res'_error _ _ _ (gid - 1)) with (@Success _ tree_res'_error s0) by admit.
+      simpl.
+      specialize (H s').
+      rewrite Heqt, Heqm0 in H.
+      rewrite Heqs' in H.
+      (* mismatching inputs... but all the match states that we manipulate here use the same input, morally *)
+      admit.
+    }
+    specialize (IHreg Hequivcont s).
+    simpl in *.
+    destruct (tm s treecont) as [t|] eqn:?; simpl; try exact I.
+    destruct (m s origcont) as [res|] eqn:?; simpl; try exact I.
+    assumption.
   - (* Unsupported *)
     split.
   - (* Unsupported *)
@@ -505,3 +550,4 @@ Proof.
     intro. destruct (let! _ =<< _ in _); split.
   - (* Negative lookbehind: unsupported *)
     intro. destruct (let! _ =<< _ in _); split.
+Admitted.
