a57d6527b1eed42fff73599bb27cadbfcb9a746b
Victor Deng <victor.deng@epfl.ch>
2025-04-25 10:39:47 +0200

    Further document code, rename LWEquivMain.v into LWEquivFrontend.v

--- warblreequiv/ListLemmas.v
+++ warblreequiv/ListLemmas.v
@@ -2,6 +2,10 @@ From Warblre Require Import List.
 From Coq Require Import List Lia ZArith.
 Import ListNotations.
 
+(** * Lemmas on lists *)
+
+(* Subtracting one to each element of a range is equivalent to subtracting one to the
+   base of the range, provided that this base is at least 1. *)
 Lemma decr_range: forall base l: nat,
   base >= 1 -> List.map (fun x => x-1) (List.List.Range.Nat.Length.range base l) =
     List.List.Range.Nat.Length.range (base-1) l.
@@ -15,9 +19,11 @@ Proof.
 Qed.
 
 
+(* Provided i1 and i2 are valid indices in a list l, if skipn i1 l = skipn i2 l,
+   then i1 = i2 (modulo conversions between Z and nat). *)
 Lemma skipn_ind_inv {A: Type}:
   forall (i1 i2: Z) (l: list A),
-    (0 <= i1)%Z -> (i1 <= Z.of_nat (length l))%Z -> (0 <= i2)%Z -> (i2 <= Z.of_nat (length l))%Z ->
+    (0 <= i1 <= Z.of_nat (length l))%Z -> (0 <= i2 <= Z.of_nat (length l))%Z ->
     skipn (Z.to_nat i1) l = skipn (Z.to_nat i2) l ->
     i1 = i2.
 Proof.
@@ -28,6 +34,8 @@ Proof.
 Qed.
 
 
+(* Modulo conversions between Z and nat, if rev pref ++ next = str0, then
+   skipn (length pref) str0 = next. *)
 Lemma skipn_lenpref_input {A: Type}:
   forall (pref next: list A) (str0: list A) (endInd1: Z),
     rev pref ++ next = str0 ->
