911bffa2e75441bc52a3ec020f4b506707edd0b1
Victor Deng <victor.deng@epfl.ch>
2025-05-06 15:31:15 +0200

    Add many cases of regexp equivalence corresponding to character descriptors

--- warblreequiv/RegexpTranslation.v
+++ warblreequiv/RegexpTranslation.v
@@ -1,4 +1,5 @@
 From Warblre Require Import Patterns Result Errors Coercions Notation Base.
+From Warblre Require Characters.
 From Linden Require Import Regex LindenParameters Chars.
 Import Notation.
 Import Result.
@@ -46,14 +47,85 @@ Inductive equiv_anchor: Patterns.Regex -> anchor -> Prop :=
 | Equiv_input_end: equiv_anchor Patterns.InputEnd EndInput
 | Equiv_word_boundary: equiv_anchor Patterns.WordBoundary WordBoundary
 | Equiv_non_word_boundary: equiv_anchor Patterns.NotWordBoundary NonWordBoundary
-. (* TODO Word boundary and non-word boundary *)
+.
+
+
+(** ** Equivalence of AtomEscapes that correspond to character classes (not backreferences). *)
+(* CharacterClassEscape *)
+Inductive equiv_CharacterClassEscape: Patterns.CharacterClassEscape -> char_descr -> Prop :=
+| Equiv_esc_d: equiv_CharacterClassEscape Patterns.esc_d CdDigits
+| Equiv_esc_D: equiv_CharacterClassEscape Patterns.esc_D (CdInv CdDigits)
+| Equiv_esc_s: equiv_CharacterClassEscape Patterns.esc_s CdWhitespace
+| Equiv_esc_S: equiv_CharacterClassEscape Patterns.esc_S (CdInv CdWhitespace)
+| Equiv_esc_w: equiv_CharacterClassEscape Patterns.esc_w CdWordChar
+| Equiv_esc_W: equiv_CharacterClassEscape Patterns.esc_W (CdInv CdWordChar)
+.
+(* TODO Unicode properties *)
+
+(* ControlEscape *)
+Inductive equiv_ControlEscape: Patterns.ControlEscape -> char_descr -> Prop :=
+| Equiv_esc_f: equiv_ControlEscape Patterns.esc_f (CdSingle Characters.FORM_FEED)
+| Equiv_esc_n: equiv_ControlEscape Patterns.esc_n (CdSingle Characters.LINE_FEED)
+| Equiv_esc_r: equiv_ControlEscape Patterns.esc_r (CdSingle Characters.CARRIAGE_RETURN)
+| Equiv_esc_t: equiv_ControlEscape Patterns.esc_t (CdSingle Characters.CHARACTER_TABULATION)
+| Equiv_esc_v: equiv_ControlEscape Patterns.esc_v (CdSingle Characters.LINE_TABULATION). (* TODO *)
+
+(* AsciiControlEsc *)
+Inductive equiv_asciiesc: AsciiLetter -> char_descr -> Prop :=
+. (* TODO *)
+
+(* CharacterEscape *)
+Inductive equiv_CharacterEscape: Patterns.CharacterEscape -> char_descr -> Prop :=
+| Equiv_ControlEsc: forall esc cd, equiv_ControlEscape esc cd -> equiv_CharacterEscape (Patterns.ControlEsc esc) cd
+| Equiv_AsciiControlEsc: forall l cd, equiv_asciiesc l cd -> equiv_CharacterEscape (Patterns.AsciiControlEsc l) cd
+| Equiv_esc_Zero: equiv_CharacterEscape Patterns.esc_Zero (CdSingle (char_from_numeric_value 0))
+(* | Equiv_HexEscape: TODO | Equiv_UnicodeEsc: TODO | Equiv_IdentityEsc: TODO *)
+.
+
+
+(** ** Equivalence of character classes *)
+Inductive equiv_ClassEscape: Patterns.ClassEscape -> char_descr -> Prop :=
+| Equiv_esc_b: equiv_ClassEscape Patterns.esc_b (CdSingle Characters.BACKSPACE)
+| Equiv_esc_Dash: equiv_ClassEscape Patterns.esc_Dash (CdSingle Characters.HYPHEN_MINUS)
+| Equiv_CCharacterClassEsc: forall esc cd, equiv_CharacterClassEscape esc cd -> equiv_ClassEscape (Patterns.CCharacterClassEsc esc) cd
+| Equiv_CCharacterEsc: forall esc cd, equiv_CharacterEscape esc cd -> equiv_ClassEscape (Patterns.CCharacterEsc esc) cd.
+
+Inductive equiv_ClassAtom: Patterns.ClassAtom -> char_descr -> Prop :=
+| Equiv_SourceCharacter: forall c: Char, equiv_ClassAtom (Patterns.SourceCharacter c) (CdSingle c) (* TODO Check *)
+| Equiv_ClassEsc: forall esc cd, equiv_ClassEscape esc cd -> equiv_ClassAtom (Patterns.ClassEsc esc) cd.
+
+Inductive equiv_ClassRanges: Patterns.ClassRanges -> char_descr -> Prop :=
+| Equiv_EmptyCR: equiv_ClassRanges Patterns.EmptyCR CdEmpty
+| Equiv_ClassAtomCR: forall ca cacd t tcd, equiv_ClassAtom ca cacd -> equiv_ClassRanges t tcd -> equiv_ClassRanges (Patterns.ClassAtomCR ca t) (CdUnion cacd tcd)
+| Equiv_RangeCR:
+  forall l h cl ch t tcd,
+    equiv_ClassAtom l (CdSingle cl) -> equiv_ClassAtom h (CdSingle ch) ->
+    char_numeric_value cl <= char_numeric_value ch ->
+    equiv_ClassRanges t tcd ->
+    equiv_ClassRanges (Patterns.RangeCR l h t) (CdUnion (CdRange cl ch) tcd).
+
+Inductive equiv_CharClass: Patterns.CharClass -> char_descr -> Prop :=
+| Equiv_NoninvertedCC: forall crs cd, equiv_ClassRanges crs cd -> equiv_CharClass (Patterns.NoninvertedCC crs) cd
+| Equiv_InvertedCC: forall crs cd, equiv_ClassRanges crs cd -> equiv_CharClass (Patterns.InvertedCC crs) (CdInv cd).
+
 
 (* equiv_regex' wreg lreg n means that the two regexes wreg and lreg are equivalent, where the number of left capturing parentheses before wreg/lreg is n. *)
 Inductive equiv_regex': Patterns.Regex -> regex -> nat -> Prop :=
 | Equiv_empty: forall n: nat, equiv_regex' Patterns.Empty Epsilon n
 | Equiv_char: forall (n: nat) (c: Char), equiv_regex' (Patterns.Char c) (Character (Chars.CdSingle c)) n
-(* Dot is not axiomatized for now in Linden.
-| Equiv_dot: forall n: nat, equiv_regex' Patterns.Dot (Character Chars.dot) n *)
+| Equiv_dot: forall n: nat, equiv_regex' Patterns.Dot (Character Chars.CdDot) n
+| Equiv_atom_CharacterClassEscape:
+  forall (esc: Patterns.CharacterClassEscape) (cd: char_descr) (n: nat),
+    equiv_CharacterClassEscape esc cd ->
+    equiv_regex' (Patterns.AtomEsc (Patterns.ACharacterClassEsc esc)) (Character cd) n
+| Equiv_atom_CharacterEscape:
+  forall (esc: Patterns.CharacterEscape) (cd: char_descr) (n: nat),
+    equiv_CharacterEscape esc cd ->
+    equiv_regex' (Patterns.AtomEsc (Patterns.ACharacterEsc esc)) (Character cd) n
+| Equiv_CharacterClass:
+  forall (cc: Patterns.CharClass) (cd: char_descr) (n: nat),
+    equiv_CharClass cc cd ->
+    equiv_regex' (Patterns.CharacterClass cc) (Character cd) n
 | Equiv_disj: forall n wr1 wr2 lr1 lr2,
     equiv_regex' wr1 lr1 n -> equiv_regex' wr2 lr2 (num_groups lr1 + n) ->
     equiv_regex' (Patterns.Disjunction wr1 wr2) (Disjunction lr1 lr2) n
