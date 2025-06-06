fa8acfb1b75f29c4eaf7535eef801d62f7546617
Victor Deng <victor.deng@epfl.ch>
2025-06-06 15:54:21 +0200

    Document main equivalence theorems

--- WarblreEquiv/EquivMain.v
+++ WarblreEquiv/EquivMain.v
@@ -13,14 +13,20 @@ Import Result.Notations.
 
 Local Open Scope result_flow.
 
+(** * Main equivalence theorems, linking everything together *)
+
 Section EquivMain.
   Context `{characterClass: Character.class}.
 
+  (** ** Some initialization results *)
   Section InitState.
+
+    (* Initial match state corresponding to a string, an index and a RegExpRecord. *)
     Definition init_match_state (str0: string) (idx: nat) (rer: RegExpRecord): MatchState :=
       let cap := List.repeat undefined (RegExpRecord.capturingGroupsCount rer) in
       match_state str0 (Z.of_nat idx) cap.
     
+    (* Such an initial match state is equivalent to the empty group map... *)
     Lemma init_ms_equiv_empty:
       forall str0 idx rer, equiv_groupmap_ms GroupMap.empty (init_match_state str0 idx rer).
     Proof.
@@ -29,6 +35,7 @@ Section EquivMain.
       rewrite nth_repeat. constructor.
     Qed.
 
+    (* ... and to the empty open group list. *)
     Lemma empty_gm_equiv_empty_gl:
       group_map_equiv_open_groups GroupMap.empty nil.
     Proof.
@@ -36,21 +43,27 @@ Section EquivMain.
       split; try discriminate. intro H. inversion H.
     Qed.
 
+    (* The initial input corresponding to a string str0 is compatible with str0. *)
     Lemma init_input_compat:
       forall str0, input_compat (init_input str0) str0.
     Proof.
       intro str0. unfold init_input. constructor. reflexivity.
     Qed.
 
+    (* The initial input corresponding to a string str0 and the initial match state
+       corresponding to str0 and index 0 match each other. *)
     Lemma init_ms_matches_inp:
       forall str0 rer,
         ms_matches_inp (init_match_state str0 0 rer) (init_input str0).
     Proof.
       intros str0 rer. unfold init_match_state, init_input. constructor; reflexivity.
     Qed.
+
   End InitState.
 
 
+  (* Computing the RegExpRecord corresponding to the flags supported by our equivalence
+     proof and containing the right number of capturing groups. *)
   Definition computeRer (wroot: Regex): RegExpRecord :=
     {|
       RegExpRecord.ignoreCase := false;
@@ -60,12 +73,18 @@ Section EquivMain.
       RegExpRecord.capturingGroupsCount := StaticSemantics.countLeftCapturingParensWithin wroot nil
     |}.
   
+
+  (** ** Plugging the identity MatcherContinuation into a Matcher compiled from some root regexp *)
   Theorem equiv_matcher_idmcont_compsucc:
     forall wroot lroot rer m,
+      (* For all pairs of equivalent Warblre and Linden regexes, *)
       equiv_regex wroot lroot ->
       rer = computeRer wroot ->
+      (* if compiling the Warblre regex into a Matcher m succeeds, *)
       Semantics.compileSubPattern wroot nil rer forward = Success m ->
       forall str0,
+        (* then plugging the identity MatcherContinuation into the Matcher m yields a
+           MatcherContinuation that is equivalent to matching the Linden regex, under any input string. *)
         equiv_cont (fun ms => m ms id_mcont) nil (forbidden_groups lroot) [Areg lroot] forward str0 rer.
   Proof.
     intros wroot lroot rer m Hequiv Heqrer Hcompsucc str0.
@@ -81,15 +100,24 @@ Section EquivMain.
     - apply List.Disjoint_nil_r.
   Qed.
 
+  (** ** Corollary assuming that both compiling the regex and matching a string do not yield errors. *)
   Corollary equiv_main':
     forall wroot lroot rer m,
+      (* For all pairs of equivalent Warblre and Linden regexes, *)
       equiv_regex wroot lroot ->
       rer = computeRer wroot ->
+      (* if compiling the Warblre regex into a matcher m succeeds, *)
       Semantics.compilePattern wroot rer = Success m ->
+      (* then for any input string str0, *)
       forall str0 res t,
+        (* if running m on str0 successfully yields a result res, *)
         m str0 0 = Success res ->
+        (* then letting t be the priority tree corresponding to matching the Linden regex
+           on the string str0, *)
         t = compute_tr [Areg lroot] (init_input str0) GroupMap.empty forward ->
+        (* this tree respects the Linden semantics and *)
         is_tree [Areg lroot] (init_input str0) GroupMap.empty forward t /\
+        (* the result res is equivalent to the first branch of the tree. *)
         equiv_groupmap_ms_opt (first_branch t) res.
   Proof.
     intros wroot lroot rer m Hequiv Heqrer Hcompsucc str0 res t Heqres Heqt.
@@ -130,16 +158,24 @@ Section EquivMain.
     - exact Hequivm.
   Qed.
 
+  (** ** Main theorem *)
   Corollary equiv_main:
     forall wroot lroot rer str0,
+      (* For any pair of equivalent Linden and Warblre regexes, *)
       equiv_regex wroot lroot ->
       rer = computeRer wroot ->
+      (* such that the Warblre regex passes the early errors check, *)
       EarlyErrors.Pass_Regex wroot nil ->
       exists m res,
+        (* compiling the Warblre regex successfully yields a matcher m, *)
         Semantics.compilePattern wroot rer = Success m /\
+        (* for any input string str0, running m on str0 yields some result res, *)
         m str0 0 = Success res /\
+        (* and letting t be the priority tree corresponding to matching the Linden regex on str0, *)
         forall t, t = compute_tr [Areg lroot] (init_input str0) GroupMap.empty forward ->
+          (* this tree respects the Linden semantics and *)
           is_tree [Areg lroot] (init_input str0) GroupMap.empty forward t /\
+          (* the result res is equivalent to the first branch of this tree. *)
           equiv_groupmap_ms_opt (first_branch t) res.
   Proof.
     intros wroot lroot rer str0 Hequiv Heqrer HearlyErrors.
