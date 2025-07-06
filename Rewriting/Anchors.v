5e1a88c135be799709ae83d6dea824f74243901c
Clément Pit-Claudel <clement.pit-claudel@epfl.ch>
2025-07-06 16:33:18 +0200

    rewriting: Prove a corollary of the anchor lemma

--- Rewriting/Anchors.v
+++ Rewriting/Anchors.v
@@ -4,15 +4,15 @@ Section Anchors.
   Context {params: LindenParameters}.
   Context (rer: RegExpRecord).
 
-  Hypothesis ascii_word_canon:
-    forall c,
-      In c Character.ascii_word_characters ->
-      In (Character.canonicalize rer c) Character.ascii_word_characters.
+  Notation ascii_word_canon :=
+    (forall c,
+        In c Character.ascii_word_characters ->
+        In (Character.canonicalize rer c) Character.ascii_word_characters).
 
-  Hypothesis line_terminators_canon:
-    forall c,
-      In c Character.line_terminators <->
-      In (Character.canonicalize rer c) Character.line_terminators.
+  Notation line_terminators_canon :=
+    (forall c,
+        In c Character.line_terminators <->
+          In (Character.canonicalize rer c) Character.line_terminators).
 
   Definition CdList (cs: list Parameters.Character) :=
     List.fold_right (fun c cd => CdUnion (CdSingle c) cd) CdEmpty cs.
@@ -58,14 +58,14 @@ Section Anchors.
     all: try rewrite Utils.List.inb_cons, IHcs; reflexivity.
   Qed.
 
-  Lemma char_match_line_terminators c:
+  Lemma char_match_line_terminators c (Hlc: line_terminators_canon):
     char_match rer c (CdList Character.line_terminators) =
       Utils.List.inb c Character.line_terminators.
   Proof.
     rewrite char_match_cdlist; unfold inb_canonicalized.
     apply Bool.eq_iff_eq_true; rewrite !Utils.List.inb_spec, in_map_iff.
     split; [ intros (c' & Hc & HIn) | solve[eauto] ].
-    rewrite line_terminators_canon in *; congruence.
+    rewrite Hlc in *; congruence.
   Qed.
 
   (* FIXME why do I need this? *)
@@ -92,7 +92,7 @@ Section Anchors.
     intuition.
   Qed.
 
-  Lemma wordCharacters_canonical t:
+  Lemma wordCharacters_canonical t (Hac: ascii_word_canon):
     CharSet.CharSetExt.contains (wordCharacters rer) t =
       CharSet.CharSetExt.exist_canonicalized rer (wordCharacters rer) (Character.canonicalize rer t).
   Proof.
@@ -103,25 +103,34 @@ Section Anchors.
     setoid_rewrite wordCharacters_spec.
     setoid_rewrite CharSet.CharSetExt.from_list_spec.
     split; [ | intros (c & HIn & <-) ];
-      intuition eauto using ascii_word_canon.
+      intuition eauto.
   Qed.
 
-  Lemma char_match_word_char c:
+  Lemma char_match_word_char c (Ha: ascii_word_canon):
     char_match rer c CdWordChar =
       CharSet.CharSetExt.contains (wordCharacters rer) c.
   Proof.
     unfold char_match; simpl.
-    symmetry; apply wordCharacters_canonical.
+    symmetry; eauto using wordCharacters_canonical.
   Qed.
 
-  Hint Rewrite char_match_line_terminators char_match_word_char : tree_equiv_symbex.
+  Hint Rewrite char_match_line_terminators char_match_word_char using eauto : tree_equiv_symbex.
 
   Theorem desugar_anchor_correct (a: anchor):
+    ascii_word_canon ->
+    line_terminators_canon ->
     Anchor a ≅[rer] desugar_anchor a.
   Proof.
-    destruct a; tree_equiv_rw.
+    intros; destruct a; tree_equiv_rw.
     all: destruct dir; tree_equiv_symbex.
-    all: try reflexivity.
+    all: reflexivity.
+  Qed.
+
+  Corollary desugar_anchor_correct_noi (a: anchor):
+    rer.(RegExpRecord.ignoreCase) = false ->
+    Anchor a ≅[rer] desugar_anchor a.
+  Proof.
+    intros Hic; apply desugar_anchor_correct.
+    all: intros c; rewrite canonicalize_casesenst; intuition.
   Qed.
 End Anchors.
-
